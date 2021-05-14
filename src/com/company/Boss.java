be4db11faeff524e59d96bfa09d5a768049650d0
hwangevgeniya <hwang.evgeniya@gmail.com>
2021-05-14 15:13:52 +0600

    ДЗ

--- src/com/company/Boss.java
+++ src/com/company/Boss.java
@@ -19,38 +19,4 @@ public class Boss extends GameEntity{
         this.bossWeapon = bossWeapon;
     }
 
-/*private int health;
-    private int damage;
-    private String defenceType;
-
-    public Boss(int health, int damage, String defenceType){
-
-        this.health = health;
-        this.damage = damage;
-        this.defenceType = defenceType;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void setHealth(int health) {
-        this.health = health;
-    }
-
-    public int getDamage() {
-        return damage;
-    }
-
-    public void setDamage(int damage) {
-        this.damage = damage;
-    }
-
-    public String getDefenceType() {
-        return defenceType;
-    }
-
-    public void setDefenceType(String defenceType) {
-        this.defenceType = defenceType;
-    }*/
 }
